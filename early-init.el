baadb32bc494be80273642188b20ed4ef7056428
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2024-04-22 15:52:43 -0400

    Remove removal of menubar and toolbar and scrollbars

--- early-init.el
+++ early-init.el
@@ -62,9 +62,9 @@
 (setq frame-inhibit-implied-resize t)
 
 ;; Faster to disable these here (before they've been initialized)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+;; (push '(menu-bar-lines . 0) default-frame-alist)
+;; (push '(tool-bar-lines . 0) default-frame-alist)
+;; (push '(vertical-scroll-bars) default-frame-alist)
 (when (featurep 'ns)
   (push '(ns-transparent-titlebar . t) default-frame-alist))
 (setq-default mode-line-format nil)
